9840ef8c7f69e1ba087206b733fda40eaf82b629
Bhavaja <151663840+Bhavaja@users.noreply.github.com>
2025-09-08 03:28:05 +0530

    Update SignupLoginPageTest.java

--- Capstone_Automation_Exercise/src/test/java/tests/SignupLoginPageTest.java
+++ Capstone_Automation_Exercise/src/test/java/tests/SignupLoginPageTest.java
@@ -35,14 +35,14 @@ public class SignupLoginPageTest {
     @Test(priority = 1)
     public void signupNewUser() throws InterruptedException {
     	//signupLoginPage.clickSignupLogin();
-    	signupLoginPage.enterNewName("Rh");
-    	signupLoginPage.enterNewEmail("rh@gmail.com");
+    	signupLoginPage.enterNewName("Ru");
+    	signupLoginPage.enterNewEmail("ru@gmail.com");
     	signupLoginPage.clickSignup();
     	/*Assert.assertTrue(driver.getTitle().toLowerCase().contains("signup"),
                 "User is not navigated to signup page'");*/
-    	signupLoginPage.enternewPassword("rh@123");
+    	signupLoginPage.enternewPassword("ru@123");
     	signupLoginPage.selectDOB("21", "June", "1996");
-    	signupLoginPage.enterFirstName("Rh");
+    	signupLoginPage.enterFirstName("Ru");
     	signupLoginPage.enterLastName("Sharma");
     	signupLoginPage.enterAddress("Abc colony");
     	signupLoginPage.selectCountry("India");
